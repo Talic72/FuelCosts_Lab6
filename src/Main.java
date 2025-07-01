c98620ab41606d71444463a576788a03e2625bc6
Talic72 <EMORRISWORK72@GMAIL.COM>
2025-06-30 22:19:37 -0400

    finish

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,76 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Scanner in  = new Scanner(System.in);
+        double numberOfGallons = 0.0;
+        double fuelEfficiency = 0.0;
+        double fuelPrice = 0.0;
+        double costPer100 = 0.0;
+        double fullTankDistance= 0.0;
+
+
+        boolean done = false;
+        do
+        {
+            System.out.println("Please enter the number of gallons in the tank:  ");
+            if(in.hasNextDouble())
+            {
+                numberOfGallons = in.nextDouble();
+                in.nextLine();
+                done = true;
+            }
+            else
+            {
+                String trash = in.next();
+                System.out.println("You said your number of gallons was " + trash);
+                System.out.println("You have to enter a valid amount!");
+            }
+        }while(!done);
+        done = false;
+
+        do
+        {
+            System.out.println("Please enter the vehicles fuel efficiency in mpg: ");
+            if(in.hasNextDouble())
+            {
+                fuelEfficiency = in.nextDouble();
+                in.nextLine();
+                done = true;
+            }
+            else
+            {
+                String trash = in.next();
+                System.out.println("You said your fuel efficiency was: " + trash);
+                System.out.println("You have to enter a valid amount!");
+            }
+        }while(!done);
+        done = false;
+
+        do
+        {
+            System.out.println("Please enter fuel price in dollars per gallon: ");
+            if(in.hasNextDouble())
+            {
+                fuelPrice = in.nextDouble();
+                in.nextLine();
+                done = true;
+            }
+            else
+            {
+                String trash = in.next();
+                System.out.println("You said your temp was: " + trash);
+                System.out.println("You have to enter a valid amount!");
+            }
+        }while(!done);
+
+        costPer100 = (100 / fuelEfficiency) * fuelPrice;
+        fullTankDistance = fuelEfficiency * numberOfGallons;
+        System.out.println("It would cost you $" + costPer100 + " to go 100 miles");
+        System.out.println(" The car can " + fullTankDistance + " on a full tank of gas");
+
+
+
     }
 }
